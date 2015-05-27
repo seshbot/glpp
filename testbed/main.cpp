fcba35e8cf1a98f0dd3602a3acca29676ced34d9
seshbot <seshbot-github@cechner.com>
2015-05-27 16:55:41 +0800

    adding empty testbed app

---
+++ testbed/main.cpp
@@ -0,0 +1,103 @@
+#ifdef _MSC_VER
+#  include <windows.h>
+#endif
+
+#include <glpp/glpp.h>
+#include <glpp/assets.h>
+#include <glpp/utils.h>
+#include <glpp/scene.h>
+
+#ifdef _MSC_VER
+#   include <glpp/gles2.h>
+#else
+#   include <glpp/gl2.h>
+#endif
+
+#include <algorithm>
+#include <array>
+#include <cstdlib>
+#include <map>
+
+
+namespace gl {
+#ifdef _MSC_VER
+   using namespace gles2;
+#else
+   using namespace gl2;
+#endif
+}
+
+#ifdef _MSC_VER
+int CALLBACK WinMain(
+   HINSTANCE hInstance,
+   HINSTANCE hPrevInstance,
+   LPSTR lpCmdLine,
+   int nCmdShow
+)
+#else
+int main()
+#endif
+{
+   utils::log(utils::LOG_INFO, "starting (cwd: %s)\n", utils::getcwd().c_str());
+
+
+   try {
+      bool should_reload_program = false;
+      auto key_handler = [&](glpp::context & ctx, glpp::Key key, int scancode, glpp::KeyAction action, int mods) {
+         if (key == glpp::KEY_ESCAPE && action == glpp::KEY_ACTION_PRESS)
+            ctx.win().set_should_close();
+         if (key == glpp::KEY_R && action == glpp::KEY_ACTION_PRESS)
+            should_reload_program = true;
+      };
+
+      glpp::context context(key_handler);
+
+      //
+      // main loop
+      //
+
+      double last_tick = glpp::get_time();
+
+      while (!context.win().closing())
+      {
+         if (should_reload_program) {
+            // TODO - reload shaders
+            should_reload_program = false;
+         }
+
+         double this_tick = glpp::get_time();
+         double time_since_last_tick = this_tick - last_tick;
+
+         const double MAX_TICK_SECONDS = 1. / 15.;
+         if (time_since_last_tick > MAX_TICK_SECONDS) time_since_last_tick = MAX_TICK_SECONDS;
+
+
+         //
+         // render
+         //
+
+         // TODO - render
+
+         last_tick = this_tick;
+
+         context.win().swap();
+      }
+   }
+   catch (glpp::shader_compile_error const & ex) {
+      utils::log(utils::LOG_ERROR, "%s\n", ex.what());
+      utils::log(utils::LOG_ERROR, "%s\n", ex.log().c_str());
+
+      glpp::shutdown();
+      exit(EXIT_FAILURE);
+   }
+   catch (std::exception const & ex) {
+      utils::log(utils::LOG_ERROR, "%s\n", ex.what());
+
+      glpp::shutdown();
+      exit(EXIT_FAILURE);
+   }
+
+   glpp::shutdown();
+
+   exit(EXIT_SUCCESS);
+}
